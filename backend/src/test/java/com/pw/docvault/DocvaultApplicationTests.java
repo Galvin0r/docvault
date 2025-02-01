d0c6aebcabf672b8c92e4481add66cef9c05f52f
Roman <rom.tytov@gmail.com>
2025-02-02 00:35:34 +0100

    Initial project structure

---
+++ backend/src/test/java/com/pw/docvault/DocvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.docvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
