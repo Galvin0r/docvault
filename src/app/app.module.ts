01db77057085c1f5209774f3523ba76c6cad1e91
Roman <rom.tytov@gmail.com>
2025-09-13 22:23:28 +0200

    installed primeNG and tailwind, configured theme for dark mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,35 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TestService } from './test.service';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { providePrimeNG } from 'primeng/config';
+import { ButtonModule } from 'primeng/button';
+import { SkyPreset } from '../presets';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule],
   providers: [
     TestService,
     provideHttpClient(withInterceptorsFromDi()),
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: SkyPreset,
+        options: {
+          darkModeSelector: '.app-dark'
+        }
+      },
+    }),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
